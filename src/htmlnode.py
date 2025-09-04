7f93f8b92026f0f22a3f42ca413b244f1f46b5a9
prancingmad <prancingmad@gmail.com>
2025-09-04 10:29:17 -0500

    Chapter 2: Lesson 5

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -36,4 +36,20 @@ class LeafNode(HTMLNode):
             return self.value
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
 
-    
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        if tag is None:
+            raise ValueError("ParentNode requires a tag")
+        if children is None or len(children) == 0:
+            raise ValueError("ParentNode requires at least one child")
+        super().__init__(tag=tag, value=None, children=children, props=props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("ParentNode must have a tag")
+        if not self.children:
+            raise ValueError("ParentNode must have children")
+
+        # Recursively render all children
+        inner_html = "".join(child.to_html() for child in self.children)
+        return f"<{self.tag}{self.props_to_html()}>{inner_html}</{self.tag}>"
